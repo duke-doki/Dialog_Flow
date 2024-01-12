f2dc84d9bfab1a9230f38414386ed2a9c663905e
duke-doki <145125297+duke-doki@users.noreply.github.com>
2024-01-12 17:33:20 +0300

    add DialogFlow learning script

---
+++ create_intent.py
@@ -0,0 +1,39 @@
+from google.cloud import dialogflow
+import json
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+if __name__ == '__main__':
+    with open('questions.json', 'r') as file:
+        questions_json = file.read()
+    questions = json.loads(questions_json)
+    for key, value in questions.items():
+        create_intent(
+            'tg-bot-lesson-3',
+            key,
+            value['questions'],
+            [value['answer']]
+        )
