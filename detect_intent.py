00b1cc4ad0140460020622e32be14ae96c2d5bdb
duke-doki <145125297+duke-doki@users.noreply.github.com>
2024-01-14 17:14:32 +0300

    remove prints

--- detect_intent.py
+++ detect_intent.py
@@ -11,15 +11,6 @@ def detect_intent_texts(project_id, session_id, text, language_code):
     response = session_client.detect_intent(
         request={"session": session, "query_input": query_input}
     )
-    print("=" * 20)
-    print("Query text: {}".format(response.query_result.query_text))
-    print(
-        "Detected intent: {} (confidence: {})\n".format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence,
-        )
-    )
-    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
 
     if response.query_result.intent.is_fallback:
         return response.query_result.fulfillment_text, True
